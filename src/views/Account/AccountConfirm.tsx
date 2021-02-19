9af2936839dc5e6b9ea8aaf3382e31f86bff8e33
Song <chsong07@gamil.com>
2021-02-19 16:28:31 +0900

    ・アカウントメール認証修正

--- src/views/Account/AccountConfirm.tsx
+++ src/views/Account/AccountConfirm.tsx
@@ -6,7 +6,8 @@ import { StringParam, useQueryParams } from "use-query-params";
 import { RouteComponentProps } from "react-router";
 import { BASE_URL } from "../../core/config";
 
-import { TypedAccountConfirmMutation } from "./queries";
+// import { TypedAccountConfirmMutation } from "./queries";
+import { TypedCmgtAccountConfirmMutation } from "./queries";
 
 import "./scss/index.scss";
 
@@ -24,8 +25,8 @@ const AccountConfirm: React.FC<RouteComponentProps> = ({ history }) => {
         content:
           anyErrors.length > 0
             ? anyErrors.map(error => error.message).join(" ")
-            : "You can now log in",
-        title: anyErrors.length > 0 ? "Error" : "Account confirmed",
+            : "これでログインできます",
+        title: anyErrors.length > 0 ? "エラー" : "アカウントが確認されました。",
       },
       { type: anyErrors.length > 0 ? "error" : "success", timeout: 5000 }
     );
@@ -33,16 +34,21 @@ const AccountConfirm: React.FC<RouteComponentProps> = ({ history }) => {
 
   React.useEffect(() => {
     this.accountManagerFn({
-      variables: { email: query.email, token: query.token },
+      // variables: { email: query.email, token: query.token },
+      variables: { email: query.email },
     })
       .then(result => {
-        const possibleErrors = result.data.confirmAccount.errors;
+        console.log(result);
+        // const possibleErrors = result.data.confirmAccount.errors;
+        console.log(result.data.update_account_user);
+        const possibleErrors =
+          result.data.update_account_user.returning.length > 0 ? [] : ["error"];
         displayConfirmationAlert(possibleErrors);
       })
       .catch(() => {
         const errors = [
           {
-            message: "Something went wrong while activating your account.",
+            message: "アカウントのアクティブ化中に問題が発生しました。",
           },
         ];
         displayConfirmationAlert(errors);
@@ -53,12 +59,12 @@ const AccountConfirm: React.FC<RouteComponentProps> = ({ history }) => {
   }, []);
 
   return (
-    <TypedAccountConfirmMutation>
+    <TypedCmgtAccountConfirmMutation>
       {accountConfirm => {
         this.accountManagerFn = accountConfirm;
         return <div />;
       }}
-    </TypedAccountConfirmMutation>
+    </TypedCmgtAccountConfirmMutation>
   );
 };
 
---
+++ src/views/Account/gqlTypes/CmgtAccountConfirm.ts
@@ -0,0 +1,32 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: CmgtAccountConfirm
+// ====================================================
+
+export interface CmgtAccountConfirm_update_account_user_returning {
+  __typename: "account_user";
+  email: string;
+}
+
+export interface CmgtAccountConfirm_update_account_user {
+  __typename: "account_user_mutation_response";
+  /**
+   * data of the affected rows by the mutation
+   */
+  returning: CmgtAccountConfirm_update_account_user_returning[];
+}
+
+export interface CmgtAccountConfirm {
+  /**
+   * update data of the table: "account_user"
+   */
+  update_account_user: CmgtAccountConfirm_update_account_user | null;
+}
+
+export interface CmgtAccountConfirmVariables {
+  email?: string | null;
+}
