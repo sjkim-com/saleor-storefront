1fd042a2ea328bcfde13e798e7bf9cb4dc3812bb
ui2-itoh <takehito.itoh@ui2.co.jp>
2021-03-12 18:16:40 +0900

    ・EC Intelligenceへのデータ送信タイミングを修正。

--- src/components/Recommend/Recommend.tsx
+++ src/components/Recommend/Recommend.tsx
@@ -66,66 +66,65 @@ const addProperty = (data: object, key: string, value: any) => {
 };
 
 const Recommend: React.FC<{ request: IRecommendRequest }> = ({ request }) => {
-  // 必須データを設定
-  const data = {
-    index: request.recommendType,
-    items: request.itemIds,
-    limit: request.recommendLimitCount,
-    tmpId: request.htmlTemplateId,
-    renderId: request.htmlTagId,
-  };
+  React.useEffect(() => {
+    // 必須データを設定
+    const data = {
+      index: request.recommendType,
+      items: request.itemIds,
+      limit: request.recommendLimitCount,
+      tmpId: request.htmlTemplateId,
+      renderId: request.htmlTagId,
+    };
 
-  // オプションデータを設定
-  addProperty(data, "xitem", request.exclusionItemIds);
-  addProperty(data, "shop", request.shopId);
-  addProperty(data, "category", request.categories);
-  addProperty(data, "string1", request.string1);
-  addProperty(data, "string2", request.string2);
-  addProperty(data, "string3", request.string3);
-  addProperty(data, "string4", request.string4);
-  addProperty(data, "string5", request.string5);
-  addProperty(data, "string6", request.string6);
-  addProperty(data, "string7", request.string7);
-  addProperty(data, "string8", request.string8);
-  addProperty(data, "string9", request.string9);
-  addProperty(data, "string10", request.string10);
-  addProperty(data, "string11", request.string11);
-  addProperty(data, "string12", request.string12);
-  addProperty(data, "string13", request.string13);
-  addProperty(data, "string14", request.string14);
-  addProperty(data, "string15", request.string15);
-  addProperty(data, "string16", request.string16);
-  addProperty(data, "string17", request.string17);
-  addProperty(data, "string18", request.string18);
-  addProperty(data, "string19", request.string19);
-  addProperty(data, "string20", request.string20);
-  addProperty(data, "string21", request.string21);
-  addProperty(data, "string22", request.string22);
-  addProperty(data, "string23", request.string23);
-  addProperty(data, "string24", request.string24);
-  addProperty(data, "string25", request.string25);
-  addProperty(data, "string26", request.string26);
-  addProperty(data, "string27", request.string27);
-  addProperty(data, "string28", request.string28);
-  addProperty(data, "string29", request.string29);
-  addProperty(data, "string30", request.string30);
-  addProperty(data, "number1", request.number1);
-  addProperty(data, "number2", request.number2);
-  addProperty(data, "number3", request.number3);
-  addProperty(data, "number4", request.number4);
-  addProperty(data, "number5", request.number5);
-  addProperty(data, "number6", request.number6);
-  addProperty(data, "number7", request.number7);
-  addProperty(data, "number8", request.number8);
-  addProperty(data, "number9", request.number9);
-  addProperty(data, "number10", request.number10);
+    // オプションデータを設定
+    addProperty(data, "xitem", request.exclusionItemIds);
+    addProperty(data, "shop", request.shopId);
+    addProperty(data, "category", request.categories);
+    addProperty(data, "string1", request.string1);
+    addProperty(data, "string2", request.string2);
+    addProperty(data, "string3", request.string3);
+    addProperty(data, "string4", request.string4);
+    addProperty(data, "string5", request.string5);
+    addProperty(data, "string6", request.string6);
+    addProperty(data, "string7", request.string7);
+    addProperty(data, "string8", request.string8);
+    addProperty(data, "string9", request.string9);
+    addProperty(data, "string10", request.string10);
+    addProperty(data, "string11", request.string11);
+    addProperty(data, "string12", request.string12);
+    addProperty(data, "string13", request.string13);
+    addProperty(data, "string14", request.string14);
+    addProperty(data, "string15", request.string15);
+    addProperty(data, "string16", request.string16);
+    addProperty(data, "string17", request.string17);
+    addProperty(data, "string18", request.string18);
+    addProperty(data, "string19", request.string19);
+    addProperty(data, "string20", request.string20);
+    addProperty(data, "string21", request.string21);
+    addProperty(data, "string22", request.string22);
+    addProperty(data, "string23", request.string23);
+    addProperty(data, "string24", request.string24);
+    addProperty(data, "string25", request.string25);
+    addProperty(data, "string26", request.string26);
+    addProperty(data, "string27", request.string27);
+    addProperty(data, "string28", request.string28);
+    addProperty(data, "string29", request.string29);
+    addProperty(data, "string30", request.string30);
+    addProperty(data, "number1", request.number1);
+    addProperty(data, "number2", request.number2);
+    addProperty(data, "number3", request.number3);
+    addProperty(data, "number4", request.number4);
+    addProperty(data, "number5", request.number5);
+    addProperty(data, "number6", request.number6);
+    addProperty(data, "number7", request.number7);
+    addProperty(data, "number8", request.number8);
+    addProperty(data, "number9", request.number9);
+    addProperty(data, "number10", request.number10);
 
-  console.log("<Recommend>");
-  console.log(JSON.stringify(data));
-
-  // EC Intelligence にRecommendリクエストを送信し、
-  // レスポンス(HTML形式)を<div>タグに挿入。
-  window._scq.push(["_recommend", data]);
+    // EC Intelligence にRecommendリクエストを送信し、
+    // レスポンス(HTML形式)を<div>タグに挿入。
+    window._scq.push(["_recommend", data]);
+  }, []);
 
   return <div id={request.htmlTagId} />;
 };
